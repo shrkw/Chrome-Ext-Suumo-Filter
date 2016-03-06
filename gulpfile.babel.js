54d4ff8906d6389df7981313fc4ed02cb61e9009
shrkw <shrkwh@gmail.com>
2016-03-06 23:48:01 +0900

    add zip task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,20 @@
 import gulp from 'gulp'
 import util from 'gulp-util'
 import babel from 'gulp-babel'
+import zip from 'gulp-zip'
+import jeditor from 'gulp-json-editor'
 
-var dest_dir = 'dest/';
+import fs from 'fs'
+
+const dest_dir = 'dest/';
+const version = (() => {
+  const json = JSON.parse(fs.readFileSync('./package.json'));
+  return json.version;
+})();
+const name = (() => {
+  const json = JSON.parse(fs.readFileSync('./package.json'));
+  return json.name;
+})();
 
 gulp.task('compile-js', () => {
   gulp.src("./src/**/*.{js,jsx}")
@@ -17,12 +29,15 @@ gulp.task('compile-js', () => {
     .pipe(gulp.dest(dest_dir));
 });
 
+
 gulp.task('watch', () => {
   gulp.watch(['./src/**/*'], ['build']);
 });
 
+
 gulp.task('manifest', () => {
-  gulp.src('src/manifest.json')
+  return gulp.src('src/manifest.json')
+    .pipe(jeditor({ version: version }))
     .pipe(gulp.dest(dest_dir));
 });
 gulp.task('vendor', () => {
@@ -34,6 +49,12 @@ gulp.task('resources', () => {
   gulp.src('resources/*')
     .pipe(gulp.dest(dest_dir + 'resources'));
 });
+
+gulp.task('zip', ['build'], function (cb) {
+  return gulp.src('dest/**/*')
+      .pipe(zip(`${name}-${version}.zip`))
+      .pipe(gulp.dest('build'));
+})
 
 gulp.task('build', ['compile-js', 'manifest', 'resources', 'vendor']);
 
