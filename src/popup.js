1731898362596125813784d5638fdfacb46cb5c9
shrkw <shrkwh@gmail.com>
2016-03-12 21:04:06 +0900

    add popup page

---
+++ src/popup.js
@@ -0,0 +1,14 @@
+const template = $('#tmpl').html();
+Mustache.parse(template);
+
+chrome.storage.local.get('excluding', (ret)=> {
+  const exc = ret.excluding;
+  const set = new Set(exc);
+  set.forEach((val)=> {
+    const rendered = $(Mustache.render(template, {value: val}));
+    rendered.find(".action").on("click", (event)=> {
+      console.log(event.target.dataset.val);
+    });
+    $("#message_txt").append(rendered);
+  });
+});
